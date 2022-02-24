24f8ecbc074a6865f53537e646e66813fdbc783d
rharris529 <jordandine@gmail.com>
2022-02-24 15:34:49 -0600

    initial files, created Potion constructor

---
+++ __tests__/Potion.test.js
@@ -0,0 +1,18 @@
+const Potion = require('../lib/Potion.js');
+
+test('creates a health potion obj', () =>{
+
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('creates a random potion obj', () =>{
+
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+});
